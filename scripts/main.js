6eca1d9d671972e3d221ea2285884fcb4d5315d5
Chris Rithmyxay <crithmyxay@gmail.com>
2017-12-15 15:38:28 -0500

    listed the events, need to add price range

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,44 @@
 // Main JS file for functions
+// var commerceURL = `https://app.ticketmaster.com/commerce/v2/events/${eventID}/offers.json?&apikey=HfiikFk0cS2WG5paSWFhZX4mwoAW5wpO`
+
+function getMbsEvents() {
+    return $.get(MBS_API);
+}
+
+function sortAscending(a,b) {
+    return a-b;
+}
+
+function makeParty(party) {
+    var eventsList = party._embedded.events;
+    // var eventArray = {};
+    for (i=0; i<= eventsList.length; i++) {
+        var eventArray = [];
+        var drawDate = eventsList[i].dates.start.localDate;
+        var drawName = eventsList[i].name;
+        // if (eventsList[i].priceRanges) {
+        //     eventsList[i].priceRanges[0].min + eventsList[i].priceRanges[0].max
+        // }
+        // else {
+        //     pass;
+        // }
+        var drawEvent = $('<a>', {
+            text: drawDate + ' ' + drawName,
+            href: '#'
+        });
+        eventArray.push(drawEvent);
+        $('[data-event-list]').append(eventArray);
+    }
+}
+
+
+
+function drawAllEvents() {
+    getMbsEvents()
+        .then(makeParty)
+        // .then(drawParty)
+}
+
+drawAllEvents();
+
+
