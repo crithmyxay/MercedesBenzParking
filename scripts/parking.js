a6780b7548a2046a95c5c672d6650cf9c6f300be
syivanes <sergey.ivanes@gmail.com>
2017-12-20 09:17:45 -0500

    recententers map, customizes markers with colors, labels and description text

--- scripts/parking.js
+++ scripts/parking.js
@@ -38,7 +38,7 @@ function crunchData(parkingNamesList) {
             var priceQuote = parkingNamesList[i].purchase_options[0].price.USD;
             var spotsAvailable = true;
         } else {
-            var priceQuote = false;
+            var priceQuote = "Sold" + "\n" + "Out";
             var spotsAvailable = false;
         }
 
@@ -57,37 +57,91 @@ var map;
 
 function initMap () {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 33.7554333, lng: -84.4032554}, 
-        zoom: 15
+        center: {lat: 33.7550543, lng: -84.3977729}, 
+        zoom: 17
     });
 
-    var locations = [];
+    // var locations = [];
     
-    for (var [key, value] of dict) {
-        var obj = {};
-        obj['lat'] = value.latitude;
-        obj['lng'] = value.longitude;
-        locations.push(obj);
-    };
-    console.log(dict.length);
-    console.log(locations);
+    // for (var [key, value] of dict) {
+    //     var obj = {};
+    //     obj['lat'] = value.latitude;
+    //     obj['lng'] = value.longitude;
+    //     locations.push(obj);
+    // };
 
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+    // var markers = locations.map(function(location, i) {
+    //     return new MarkerWithLabel({
+    //         position: location,
+    //         label: labels[i % labels.length]
+    //     });
+    // });
+    // var value = dict.get(4);
+
+    function addMarkerListener(marker, infoWindow, map) {
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
         });
-    });
+    }
+    var markers = [];
+    function generateMarkers() {
+        for (var [key, value] of dict) {
+            var marker = new MarkerWithLabel({            
+            
+                position: new google.maps.LatLng(value.latitude, value.longitude),
+                map: map,
+                draggable: false, 
+                raiseOnDrag: false,
+                labelContent: "$" + value.priceQuote,
+                labelAnchor: new google.maps.Point(15, 65),
+                labelClass: "labels",
+                labelInBackground: false,
+                icon: pinSymbol('blue')
+            })
+
+
+            if (value.spotsAvailable == false) {
+                marker.labelContent = value.priceQuote
+                marker.icon = pinSymbol('red')
+            }
+
+            var contentString = value.streetAddress;
+            var infoWindow = new google.maps.InfoWindow({
+                content: contentString
+            })
+            
+            addMarkerListener(marker, infoWindow, map);
+            markers.push(marker);
+        };
+        
+    }
+    generateMarkers();
+
+    console.log(markers);
+
+
+
+    function pinSymbol(color) {
+        return {
+            path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z',
+            fillColor: color,
+            fillOpacity: 1,
+            strokeColor: '#000',
+            strokeWeight: 1,
+            scale: 2,
+            fontSize: "8px"
+        };
+    }
 
     var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
 }
-
 function mapParkingResults() {
     findParking()
         .then(initMap);
 }
 mapParkingResults();
+// $(document).ready(google.maps.event.addDomListener(window, 'load', initMap));
 
